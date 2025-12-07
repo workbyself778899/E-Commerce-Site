7c0ff240dbdaef0adbf9469a8011f037f00d4485
Ayush <workbyself778899@gmail.com>
2025-12-07 12:01:04 +0545

    v0.4

--- backend/index.js
+++ backend/index.js
@@ -14,39 +14,42 @@ require("./dbConnection/dbConnect");
 
 // Using api which is inside the user file
 const user = require('./routes/user');
-app.use('/user',user)
+app.use('/user', user)
 
 const blog = require('./routes/blog');
-app.use('/blog',blog)
+app.use('/blog', blog)
 
 const product = require('./routes/product');
-app.use('/product',product)
+app.use('/product', product)
 
 const order = require('./routes/order')
-app.use('/order',order)
+app.use('/order', order)
 
 const contact = require('./routes/contact')
-app.use('/contact',contact)
+app.use('/contact', contact)
 // Featured routes (Top picks / Here table)
 const featured = require('./routes/featured')
 app.use('/featured', featured)
-    
-app.get('/',(req,res)=>{
+
+app.get('/', (req, res) => {
     res.send("Show the code")
 })
 
 
 
 try {
-    app.listen(process.env.PORT,()=>{
-    console.log("server running");
+
     app.use(cors({
-  origin: "*",
-  methods: "GET,POST,PUT,DELETE,OPTIONS",
-  allowedHeaders: "Content-Type,Authorization",
-}));
+        origin: "*",
+        methods: "GET,POST,PUT,DELETE,OPTIONS",
+        allowedHeaders: "Content-Type,Authorization",
+    }));
     app.options("*", cors());
-})
+
+    app.listen(process.env.PORT, () => {
+        console.log("server running");
+    })
+    
 } catch (error) {
     console.log(error)
 }
